e5fad2c402e8f62a7499e8aae857087a8697bf5e
Lucas Suares <hub_suares.01@hotmail.com>
2022-12-17 15:11:23 -0300

    feat(): setup bootstrap file

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000);
+}
+bootstrap();

4ef6f716147174342d04ed2749abdfd17a29ee15
Lucas Suares <hub_suares.01@hotmail.com>
2022-12-17 14:55:39 -0300

    feat(): create send notification use case

---
+++ src/app/use-cases/send-notification.ts
@@ -0,0 +1,37 @@
+import { Content } from '@app/entities/notification/content';
+import { Notification } from '@app/entities/notification/notification';
+import { NotificationsRepository } from '@app/repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+@Injectable()
+export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { category, content, recipientId } = request;
+
+    const notification = new Notification({
+      category,
+      content: new Content(content),
+      recipientId,
+    });
+
+    await this.notificationsRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
